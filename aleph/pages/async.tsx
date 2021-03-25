32745c4f551a273a07a1dacead7e5eb6aa697509
imjwang <ngjaywa@gmail.com>
2021-03-25 16:33:15 -0400

    working calls to server

--- aleph/pages/async.tsx
+++ aleph/pages/async.tsx
@@ -4,20 +4,18 @@ import Items from '../components/items.tsx'
 import Nav from '../components/nav.tsx'
 import Reminder from '../components/reminder.tsx'
 
-export default function Async() { 
+export default function Post() {
+  const post = useDeno(async () => {
+    return await (await fetch("http://localhost:5000/api/products")).json();
+  });
+  const user_items = [];
+  const data = post.data;
+
+  for (const p in data) {
+    const item = data[p];
+    //TODO let users enter custom msgbody for each msgtype for each item
+    user_items.push(<Reminder key={item._id} msgtype="is-warning" item={item.name} msgbody="it's low!"/>);
+  }
   
-
-
-    const post = useDeno(async () => {
-      const { data } = await fetch("https://api.icndb.com/jokes/random")
-      .then((res) => res.json());
-    });
-    post();
-
-  return (
-    <div>
-      <h1>Here's a random joke for you</h1>
-      <h2>post</h2>
-    </div>
-  )
+  return user_items;
 }
