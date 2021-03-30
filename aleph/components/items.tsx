ebfd24d3514783c6dc2b439f217a3a499be35748
imjwang <ngjaywa@gmail.com>
2021-03-30 10:14:37 -0400

    generate table from data

--- aleph/components/items.tsx
+++ aleph/components/items.tsx
@@ -1,28 +1,29 @@
 import React from 'react'
+import { useDeno } from 'aleph'
 
 export default function Items(){
-    return(
-<table className="table is-bordered is-fullwidth">
-  <tr>
-    <th>Item</th>
-      <th>Remainder</th>
-    </tr>
-    <tr>
-      <td>Sauce</td>
-      <td>4</td>
-    </tr>
-    <tr>
-      <td>is</td>
-      <td>5</td>
-    </tr>
-    <tr>
-      <td>the</td>
-      <td>6</td>
-    </tr>
-    <tr>
-      <td>Boss</td>
-      <td>7</td>
-    </tr>
-</table>
+
+  const post = useDeno(async () => {
+    return await (await fetch("http://localhost:5000/api/products")).json();
+  });
+  const user_items = [];
+  const data = post.data;
+  
+  for (const p in data) {
+    const item = data[p];
+    user_items.push(   <tr key={item._id}>
+                    <td>{item.name}</td>
+                    <td>{item.quantity}</td>
+                </tr>)
+  }
+
+  return(
+    <table className="table is-bordered is-fullwidth">
+      <tr>
+        <th>Item</th>
+        <th>Remainder</th>
+      </tr>
+      {user_items}
+    </table>
     )
 }
